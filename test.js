1d223061ad2d9f91f097807bde5c63959d151f7b
Panasa Teja <69209797+teja963@users.noreply.github.com>
2021-01-22 19:34:15 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+<script >
+			function Teja(){
+				document.getElementById("demo").innerHTML="TRY CHESAV KADA STOP KUDA CHEY DOWN BUTTON NI PRESS CHEY";
+			}
+			function JACK(){
+				document.getElementById("demo").innerHTML="GOOD BOY STOP CHESI NADHUKU❤😘";
+			}
+		</script>
